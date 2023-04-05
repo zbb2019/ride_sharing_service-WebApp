9f19d303cfbc66e77d900a900b3418744abec840
Mila Zhao <binbin.zhao@duke.edu>
2023-04-04 23:02:31 -0400

    2. modified rider.models & added rider.templates
    In rider.models: added Order (i.e. ride request)
    In rider.templates.rider: added home.html

--- ride_sharing_service/ride_sharing_service/settings.py
+++ ride_sharing_service/ride_sharing_service/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rider.apps.RiderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ride_sharing_service/rider/models.py
+++ ride_sharing_service/rider/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Order(models.Model):  # Order = ride request
+    status = models.CharField(max_length=100)
+    ownerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    # orderID = id
+    sharableTF = models.BooleanField()
+    destAddr = models.TextField()
+    reqArrvTime = models.CharField(max_length=100)
+    ownerPartySize = models.PositiveIntegerField()
+    vehicleType = models.CharField(max_length=100, blank=True)
+    specialRequest = models.TextField(blank=True)
+
+    def __str__(self):
+        return 'order #' + str(self.id)
